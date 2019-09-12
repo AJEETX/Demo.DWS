2f32bbb9b64d6bd77618b2d51e14884df34548ff
Guest <ajeetx@email.com>
2019-09-12 10:24:51 +1000

    model data annotated

--- Server/Demo.DWS/Model/Models.cs
+++ Server/Demo.DWS/Model/Models.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace Demo.DWS.Model
     public class Input
     {
         public int ID { get; set; }
+        [Required]
         public string UnitID { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public string UnitPrice { get; set; }
     }
     public class Output
